33a21067c1d5494d610e52785dfb50a6f1589de1
Nubox <sjhbelieve@gmail.com>
2024-10-14 19:19:54 +0900

    1014_자소서 작성및 알고리즘

---
+++ Algorithm_Study/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    public class Program()
+    {
+        private int[] SolveProblem(int[] arr, int[,] queries)
+        {
+            int[] answer = new int[queries.GetLength(0)] ;
+
+            int startNum = 0;
+            int endNum = 0;
+            int compareNum = 0;
+            List<int> test = new List<int>();
+    
+
+
+            for (int i = 0; i < queries.GetLength(0); ++i)
+            {
+                startNum = queries[i, 0];
+                endNum = queries[i, 1];
+                compareNum = queries[i, 2];
+
+                int result = -1;
+                for (int j = startNum; j <= endNum; ++j)
+                {
+                    if (arr[j] > compareNum && result == -1)
+                        result = arr[j];
+                    if (result > arr[j])
+                        result = arr[j];
+                }
+                answer[i] = result;
+
+            }
+
+            return answer;
+        }
+
+
+        public int[] soluTemp(int[] arr) 
+        {
+            List<int> stk = new List<int>();
+       
+
+            for (int i =0; i < arr.Length;) 
+            {
+
+                if (stk.Count <= 0) stk.Add(arr[i++]);
+
+                if (arr[i] > stk[stk.Count - 1]) stk.Add(arr[i++]);
+                else stk.RemoveAt(stk.Count - 1);
+
+            }
+
+            return stk.ToArray();
+        }
+
+        public static void Main(string[] _args)
+        {
+            Program test = new Program();
+            int[] temp = new int[] { 5, 3, 4, 1, 2 };
+
+            temp = test.soluTemp(temp);
+
+            foreach (int i in temp) 
+            {
+                Console.Write($"{i}");
+            }
+
+        }
+    }
+
+
+
+
+}
---
+++ CodingTest/Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+
+
+int main() 
+{
+	std::array<int, 10> arr1 ;
+
+	arr1[0] = 1;
+
+	std::cout << "arr1 _1 : " << arr1[0] << std::endl;
+
+	return 0;
+
+}
